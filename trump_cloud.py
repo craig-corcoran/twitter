b471c27fb212f9e503aad0813be9c9314575946a
craig <ccor5588@gmail.com>
2017-05-12 12:10:24 -0500

    added requirements file, fixed some pep 8 formatting, and made virtualenv compatible

--- trump_cloud.py
+++ trump_cloud.py
@@ -2,36 +2,47 @@ import sys
 import twitter
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
-import matplotlib.pyplot as plt
 from wordcloud import WordCloud
+import matplotlib
 
-def trump_cloud():
 
-    df = pd.read_csv('keys.csv')
+def trump_cloud(count=200, img_filename='wordcloud.png'):
+    '''
+    Make a wordcloud from trump's most recent tweets; creates a png
+    :param count: number of tweets to use (max 200?)
+    :param img_filename: name of image file for word cloud
+    :return: None
+    '''
+
+    # HACK: this makes the png look like shit, but runs in the virtualenv (without conda)
+    matplotlib.use('TkAgg')  # must be done before importing pyplot
+    import matplotlib.pyplot as plt
+
+    key_frame = pd.read_csv('keys.csv')
 
     api = twitter.Api(
-        consumer_key=df['consumer_key'][0],
-        consumer_secret=df['consumer_secret'][0],
-        access_token_key=df['access_token'][0],
-        access_token_secret=df['access_secret'][0],
+        consumer_key=key_frame['consumer_key'][0],
+        consumer_secret=key_frame['consumer_secret'][0],
+        access_token_key=key_frame['access_token'][0],
+        access_token_secret=key_frame['access_secret'][0],
     )
 
-    statuses = api.GetUserTimeline(screen_name="realDonaldTrump", count=200)  # 200 seems to be max
-    tweets = [s.text for s in statuses]
-    cv = CountVectorizer(input='content', stop_words="english")
-    X = cv.fit_transform(tweets).toarray()
-    counts = X.sum(axis=0)
-    words = cv.get_feature_names()
+    tweets = api.GetUserTimeline(screen_name="realDonaldTrump", count=count)
+    tweets = [s.text for s in tweets]
+    counter = CountVectorizer(input='content', stop_words="english")
+    counts = counter.fit_transform(tweets).toarray().sum(axis=0)
+    words = counter.get_feature_names()
     freqs = dict(zip(words, counts))
 
-    blacklist = ['https', 'rt', 'amp']
-    for word in blacklist:
+    # remove some junk words
+    for word in ['https', 'rt', 'amp']:
         freqs.pop(word)
 
     wordcloud = WordCloud().generate_from_frequencies(freqs)
     plt.imshow(wordcloud)
     plt.axis('off')
-    plt.savefig('wordcloud.png', dpi=300)
+    plt.savefig(img_filename, dpi=500)
+
 
 if __name__ == '__main__':
     sys.exit(trump_cloud())
